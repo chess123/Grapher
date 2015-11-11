e37490c35a0d95c60aa45c39a20cf3cd2fb08e9c
chess123 <kesav@viswanadha.net>
2015-11-10 21:14:04 -0800

    Added more functionality

--- ParseExpression.java
+++ ParseExpression.java
@@ -1,14 +1,16 @@
 public class ParseExpression {
 	public static void main(String[] args) {
-		String expr = Prompt.getString("Enter an expression -> ");
+		String expr = Prompt.getString("Enter an expression for f(x) -> ");
 		double val = Prompt.getDouble("Enter a value -> ");
-		System.out.println("f(x) = " + parseEvaluate(expr, val));
+		System.out.println("f(" + val + ") = " + parseEvaluate(expr, val));
 	}
 
 	public static double parseEvaluate(String expr, double x) {
 		if (expr.charAt(0) == '(' && expr.charAt(expr.length() - 1) == ')') expr = expr.substring(1, expr.length() - 1);
 		if (expr.equals("x")) return x;
 		if (isNumber(expr)) return Double.parseDouble(expr);
+		if (expr.equals("pi")) return Math.PI;
+		if (expr.equals("e")) return Math.E;
 		int maxParen = numParens(expr);
 		int num;
 		for (int j = 0; j <= maxParen; j++) {
@@ -62,6 +64,71 @@ public class ParseExpression {
 					return Math.pow(parseEvaluate(expr1, x), parseEvaluate(expr2, x));
 				}
 			}
+			num = 0;
+			for (int i = 0; i < expr.length(); i++) {
+				if (expr.charAt(i) == '(') num++;
+				if (expr.charAt(i) == ')') num--;
+				if (i + 3 < expr.length() && expr.substring(i, i + 3).equals("sin") && num == j) {
+					String expr1 = expr.substring(i + 4, nextParen(expr, i + 4)).trim();
+					return Math.sin(parseEvaluate(expr1, x));
+				}
+				if (i + 3 < expr.length() && expr.substring(i, i + 3).equals("cos") && num == j) {
+					String expr1 = expr.substring(i + 4, nextParen(expr, i + 4)).trim();
+					return Math.cos(parseEvaluate(expr1, x));
+				}
+				if (i + 3 < expr.length() && expr.substring(i, i + 3).equals("tan") && num == j) {
+					String expr1 = expr.substring(i + 4, nextParen(expr, i + 4)).trim();
+					return Math.tan(parseEvaluate(expr1, x));
+				}
+				if (i + 3 < expr.length() && expr.substring(i, i + 3).equals("csc") && num == j) {
+					String expr1 = expr.substring(i + 4, nextParen(expr, i + 4)).trim();
+					return 1 / Math.sin(parseEvaluate(expr1, x));
+				}
+				if (i + 3 < expr.length() && expr.substring(i, i + 3).equals("sec") && num == j) {
+					String expr1 = expr.substring(i + 4, nextParen(expr, i + 4)).trim();
+					return 1 / Math.cos(parseEvaluate(expr1, x));
+				}
+				if (i + 3 < expr.length() && expr.substring(i, i + 3).equals("cot") && num == j) {
+					String expr1 = expr.substring(i + 4, nextParen(expr, i + 4)).trim();
+					return 1 / Math.tan(parseEvaluate(expr1, x));
+				}
+				if (i + 2 < expr.length() && expr.substring(i, i + 2).equals("ln") && num == j) {
+					String expr1 = expr.substring(i + 3, nextParen(expr, i + 3)).trim();
+					return Math.log(parseEvaluate(expr1, x));
+				}
+				if (i + 4 < expr.length() && expr.substring(i, i + 4).equals("sqrt") && num == j) {
+					String expr1 = expr.substring(i + 5, nextParen(expr, i + 5)).trim();
+					return Math.sqrt(parseEvaluate(expr1, x));
+				}
+				if (i + 3 < expr.length() && expr.substring(i, i + 3).equals("abs") && num == j) {
+					String expr1 = expr.substring(i + 4, nextParen(expr, i + 4)).trim();
+					return Math.abs(parseEvaluate(expr1, x));
+				}
+				if (i + 6 < expr.length() && expr.substring(i, i + 6).equals("arcsin") && num == j) {
+					String expr1 = expr.substring(i + 7, nextParen(expr, i + 7)).trim();
+					return Math.asin(parseEvaluate(expr1, x));
+				}
+				if (i + 6 < expr.length() && expr.substring(i, i + 6).equals("arccos") && num == j) {
+					String expr1 = expr.substring(i + 7, nextParen(expr, i + 7)).trim();
+					return Math.acos(parseEvaluate(expr1, x));
+				}
+				if (i + 6 < expr.length() && expr.substring(i, i + 6).equals("arctan") && num == j) {
+					String expr1 = expr.substring(i + 7, nextParen(expr, i + 7)).trim();
+					return Math.atan(parseEvaluate(expr1, x));
+				}
+				if (i + 6 < expr.length() && expr.substring(i, i + 6).equals("arccsc") && num == j) {
+					String expr1 = expr.substring(i + 7, nextParen(expr, i + 7)).trim();
+					return Math.asin(1 / parseEvaluate(expr1, x));
+				}
+				if (i + 6 < expr.length() && expr.substring(i, i + 6).equals("arcsec") && num == j) {
+					String expr1 = expr.substring(i + 7, nextParen(expr, i + 7)).trim();
+					return Math.acos(1 / parseEvaluate(expr1, x));
+				}
+				if (i + 6 < expr.length() && expr.substring(i, i + 6).equals("arccot") && num == j) {
+					String expr1 = expr.substring(i + 7, nextParen(expr, i + 7)).trim();
+					return Math.atan(1 / parseEvaluate(expr1, x));
+				}
+			}
 		}
 		return 9.9;
 	}
@@ -89,5 +156,15 @@ public class ParseExpression {
 			if (cnt > max) max = cnt;
 		}
 		return max;
+	}
+
+	public static int nextParen(String str, int index) {
+		int cntParens = 1;
+		for (int i = index + 1; i < str.length(); i++) {
+			if (str.charAt(i) == '(') cntParens++;
+			if (str.charAt(i) == ')') cntParens--;
+			if (cntParens == 0) return i;
+		}
+		return -1;
 	}
 }
